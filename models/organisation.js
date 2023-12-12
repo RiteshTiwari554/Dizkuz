b987a30461e09d340e633dd37dd19783c1092f59
Ritesh Tiwari <84981640+RiteshTiwari554@users.noreply.github.com>
2023-12-12 18:27:41 +0530

    Delete models directory

--- models/organisation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const OrganisationSchema = new Schema({
-  users: [String],
-  name: String,
-});
-
-module.exports = mongoose.model("Organisation", OrganisationSchema);
